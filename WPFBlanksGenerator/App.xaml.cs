3381cfbb2e9c00eccb62b9f639f01fbc30559bac
Vincent Neparidze <nva@akmetron.ru>
2018-01-11 11:38:56 +0300

    Added PropertyChanged and corrected styles

--- WPFBlanksGenerator/App.xaml.cs
+++ WPFBlanksGenerator/App.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WPFBlanksGenerator
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public ApplicationViewModel ApplicationViewModel { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ApplicationViewModel = new ApplicationViewModel();
+        }
     }
 }
---
+++ WPFBlanksGenerator/PropertyChanged.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFBlanksGenerator
+{
+    [Serializable]
+    public class PropertyChange : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual void OnPropertyChanged(params string[] propertyNames)
+        {
+            foreach (string propertyName in propertyNames)
+                OnPropertyChanged(propertyName);
+        }
+    }
+}
