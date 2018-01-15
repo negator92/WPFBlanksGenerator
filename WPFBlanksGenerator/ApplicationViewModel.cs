b1ad3a15db1e4d0983033d69b110be1f14f85437
Vincent Neparidze <nva@akmetron.ru>
2018-01-15 11:27:11 +0300

    Added RelayCommand class
    Added RelayCommand class for comfortable binding methods to Buttons

--- WPFBlanksGenerator/ApplicationViewModel.cs
+++ WPFBlanksGenerator/ApplicationViewModel.cs
@@ -5,5 +5,12 @@ namespace WPFBlanksGenerator
     public class ApplicationViewModel : PropertyChange, INotifyPropertyChanged
     {
         public static Solution Solution { get; set; } = new Solution();
+
+        public ICommand SetFolder { get; set; }
+
+        public ApplicationViewModel()
+        {
+            SetFolder = new RelayCommand();
+        }
     }
 }
---
+++ WPFBlanksGenerator/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFBlanksGenerator
+{
+    public class RelayCommand<T> : ICommand
+    {
+        protected Action<T> execute;
+        protected Func<T, bool> canExecute;
+
+        public RelayCommand(Action<T> execute) : this(execute, (Func<T, bool>)null) { }
+
+        public RelayCommand(Action<T> execute, Func<bool> canExecute) : this(execute, t => canExecute()) { }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute ?? (t => true);
+        }
+
+        public RelayCommand() { }
+
+        public void Execute(object parameter) => execute((T)parameter);
+        public bool CanExecute(object parameter) => canExecute((T)parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    public class RelayCommand : RelayCommand<object>
+    {
+        public RelayCommand(Action execute) : base(t => execute()) { }
+        public RelayCommand(Action<object> execute) : base(execute) { }
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute) : base(execute, canExecute) { }
+        public RelayCommand(Action execute, Func<bool> canExecute) : base(t => execute(), t => canExecute()) { }
+        public RelayCommand(Action<object> execute, Func<bool> canExecute) : base(execute, canExecute) { }
+    }
+}
