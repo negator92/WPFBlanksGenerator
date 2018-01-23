97c02ab0892b504c74a7062eeacd2936fcc8b552
Vincent Neparidze <nva@akmetron.ru>
2018-01-23 10:46:33 +0300

    Try to add choosing sln button

--- WPFBlanksGenerator/ApplicationViewModel.cs
+++ WPFBlanksGenerator/ApplicationViewModel.cs
@@ -1,27 +1,49 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
+using MessageBox = System.Windows.MessageBox;
 
 namespace WPFBlanksGenerator
 {
     public class ApplicationViewModel : PropertyChange, INotifyPropertyChanged
     {
+        private const string OpenFileDialogFilter = "Solution file|*.sln";
+        private const string OpenFileDialogTitle = "Select a Solution file with WPF project";
+
         public static Solution Solution { get; set; } = new Solution();
 
-        public ICommand SetFolderCommand { get; set; }
+        public ICommand SetSlnCommand { get; set; }
 
         public ApplicationViewModel()
         {
-            SetFolderCommand = new RelayCommand(SetFolder, () => true);
+            SetSlnCommand = new RelayCommand(SetFolder, () => true);
         }
 
         private void SetFolder()
         {
-            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                DialogResult result = dialog.ShowDialog();
-                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
-                    Solution.Path = dialog.SelectedPath;
+                openFileDialog.DefaultExt = OpenFileDialogFilter.Substring(14, 4);
+                openFileDialog.Filter = OpenFileDialogFilter;
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog.Multiselect = false;
+                openFileDialog.Title = OpenFileDialogTitle;
+
+                if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                    return;
+                
+                try
+                {
+                    if (File.Exists(openFileDialog.FileName))
+                        Solution.Path = openFileDialog.FileName;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"{e.Message}\n{e.StackTrace}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
             MessageBox.Show($"You choose {Solution.Path}");
         }
